33f9ba1fb58e61ce25ec68454c5728afee32b7d2
Henok <henok_206@yahoo.com>
2023-12-19 05:13:22 +0300

    update firbase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getAuth } from "firebase/auth";
+
 //from maru current update
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
