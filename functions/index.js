7fc039f353fd61b9bc6870b42e744872c71bebf9
Henok <henok_206@yahoo.com>
2023-12-23 01:45:25 +0300

    adding resolution in packajson

--- functions/index.js
+++ functions/index.js
@@ -3,34 +3,34 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-    "sk_test_51OPPwVFIMHZ2ryul7S6PmN4IFvzb4IMhlSlUVuG5DAgVfdmsqUY6CMAjEdivp9toqIskHk7vGmWWDGSUvBIst5QA001ollhNqN"
+    "sk_test_51OPPwVFIMHZ2ryul7S6PmN4IFvzb4IMhlSlUVuG5DAgVfdmsqUY6CMAjEdivp9toqIskHk7vGmWWDGSUvBIst5QA001ollhNqN",
 );
 
 // App config
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: true }));
+app.use(cors({origin: true}));
 app.use(express.json());
 
 // Routes
 app.get("/", (request, response) => {
-    response.status(200).send("Hello World");
+  response.status(200).send("Hello World");
 });
 
 app.post("/payments/create", async (request, response) => {
-    const total = request.query.total;
-    console.log("Payment Request Received for this amount >>>>>>>>", total);
+  const total = request.query.total;
+  console.log("Payment Request Received for this amount >>>>>>>>", total);
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: "USD",
-    });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "USD",
+  });
 
-    // Created
-    response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    });
+  // Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
 });
 
 // Listen command
