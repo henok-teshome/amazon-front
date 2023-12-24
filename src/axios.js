b8fbd2f9009a2a17554cc3a2bba33f126598997b
Henok <henok_206@yahoo.com>
2023-12-24 10:54:43 +0300

    update axios

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5001/dec-22edb/us-central1/api",
+    baseURL: "http://127.0.0.1:10000",
 });
 export default instance;
